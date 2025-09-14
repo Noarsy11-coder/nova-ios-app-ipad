097e20defab653b1583d2b00a1aca0d2dfe87204
Luis Garza <97556340+Luisg0527@users.noreply.github.com>
2025-09-14 13:21:25 -0600

    Commit inicial

---
+++ nova-ios-app-ipad/nova-ios-app-ipad/nova_ios_app_ipadApp.swift
@@ -0,0 +1,17 @@
+//
+//  nova_ios_app_ipadApp.swift
+//  nova-ios-app-ipad
+//
+//  Created by Luis Garza on 11/09/25.
+//
+
+import SwiftUI
+
+@main
+struct nova_ios_app_ipadApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
